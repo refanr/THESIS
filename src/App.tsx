d1a2cf6040cd026057b9190243147455ae5ef05c
Reynir Gunnarsson <reynirg20@ru.is>
2024-01-11 20:06:36 +0000

    initializing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='main'>
+      <header>
+        <h1>Lokaverkefni BA - Hafrún Hafþórsdóttir og Kristjana Dögg Hafþórsdóttir</h1>
+      </header>
+      <main>
+        <h1>Meginmál</h1>
+        <h2>Topic</h2>
+        <p>Hérna verður eitthvað um eitthvað</p>
+      </main>
+      <footer>
+        <p>2024 - Hafrún Hafþórsdóttir og Kristjana Dögg Hafþórsdóttir</p>
+      </footer>
+    </div>
   )
 }
 
